27d3ad9f8b533839b90c7ed89272899e39a84346
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-02-12 19:21:27 +0100

    Ea sports it's in the game

--- OrderFood/OrderFood.py
+++ OrderFood/OrderFood.py
@@ -45,7 +45,6 @@ class CTkOptionMenu(tkinter.OptionMenu):
         return self._var.get()
 
 
-
 """ 
 
 class App(customtkinter.CTk):
@@ -102,7 +101,7 @@ class App(customtkinter.CTk):
         self.FoodL.grid(row=0, column=0, padx = 20, pady = 10, sticky="nsew")
 
         foodOptions = [(food.name, food.price) for food in foods]
-        self.FoodOM = CTkOptionMenu(self, values=foodOptions, command=self.optionmenuCallback)
+        self.FoodOM = CTkOptionMenu(self, values=foodOptions, command=lambda x: self.optionmenuCallback(x, 'food'))
         self.FoodOM.grid(row=1, column=0, padx=10, pady = 10, sticky="n")
     
 
@@ -110,24 +109,39 @@ class App(customtkinter.CTk):
         self.DrinkL.grid(row=0, column=1, padx = 20, pady = 10, sticky="nsew")
         
         drinkOptions = [(drink.name, drink.price) for drink in drinks]
-        self.DrinkOM = CTkOptionMenu(self, values=drinkOptions, command=self.optionmenuCallback)
+        self.DrinkOM = CTkOptionMenu(self, values=drinkOptions, command=lambda x: self.optionmenuCallback(x, 'drink'))
         self.DrinkOM.grid(row=1, column=1, padx=10, pady = 10, sticky="n")
 
         self.DessertL = customtkinter.CTkLabel(master=self, text="Dessert:", font= (None, 18), width = 10)
         self.DessertL.grid(row=0, column=2, padx = 20, pady = 10, sticky="nsew")
 
         dessertOptions = [(dessert.name, dessert.price) for dessert in desserts]
-        self.DessertOM = CTkOptionMenu(self, values=dessertOptions, command=self.optionmenuCallback)
+        self.DessertOM = CTkOptionMenu(self, values=dessertOptions, command=lambda x: self.optionmenuCallback(x, 'dessert'))
         self.DessertOM.grid(row=1, column=2, padx=10, pady = 10, sticky="n")
 
+        self.stringvar = tkinter.StringVar()
 
+        self.textbox = customtkinter.CTkTextbox(master=self, width=30, height=100, corner_radius=5)
+        self.textbox.grid(row=2, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+        self.textbox.insert("0.0", "Some example text!\n")
+        
     # add methods to app
     def buttonClick(self):
         print("button click")
 
-    def optionmenuCallback(self, choice):
-        print("You have picked:", choice)
+ 
+    def optionmenuCallback(self, choice, item_type):
+        if item_type == 'food':
+            items = foods
+        elif item_type == 'drink':
+            items = drinks
+        elif item_type == 'dessert':
+            items = desserts
 
+        for item in items:
+            if item.name == choice:
+                self.textbox.insert("end", f"{item.name} {item.price}kr ")
+        print("You have picked:", choice)
 
 
 app = App()
