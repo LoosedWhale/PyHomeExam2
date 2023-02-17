a48bd1f41555fcbee013527164468c2c2106e884
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-02-17 18:11:25 +0100

    Update SchackKod.py

--- Code/SchackKod.py
+++ Code/SchackKod.py
@@ -16,7 +16,7 @@ class Square:
         self.col = col
         self.piece = piece
         
-    def update_piece(self, piece):
+    def updatePiece(self, piece):
         self.piece = piece
 
 # define a base class for all pieces
@@ -25,15 +25,15 @@ class Piece:
         self.color = color
         self.position = position
         
-    def move(self, new_position):
-        self.position = new_position
+    def move(self, newPosition):
+        self.position = newPosition
 
 # define a class for pawn
 class Pawn(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         
-    def move(self, new_position):
+    def move(self, newPosition):
         pass
 
 # define a class for knight
@@ -41,7 +41,7 @@ class Knight(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         
-    def move(self, new_position):
+    def move(self, newPosition):
         pass
 
 # define a class for bishop
@@ -50,7 +50,7 @@ class Bishop(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
 
-    def move(self, new_position):
+    def move(self, newPosition):
         pass
 
 # define a class for rook
@@ -58,21 +58,21 @@ class Rook(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         
-    def move(self, new_position):
+    def move(self, newPosition):
         pass
 # define a class for queen
 class Queen(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         
-    def move(self, new_position):
+    def move(self, newPosition):
         pass
 # define a class for king
 class King(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         
-    def move(self, new_position):
+    def move(self, newPosition):
         pass
 
 # define a class for the chess board and set up the pieces on the board. 
@@ -81,35 +81,35 @@ class ChessBoard:
         self.board = [[Square(i, j) for j in range(8)] for i in range(8)]
         
         for col in range(8):
-            self.board[1][col].update_piece(Pawn('black', (1, col)))
-            self.board[6][col].update_piece(Pawn('white', (6, col)))
+            self.board[1][col].updatePiece(Pawn('black', (1, col)))
+            self.board[6][col].updatePiece(Pawn('white', (6, col)))
         
-        self.board[0][0].update_piece(Rook('black', (0, 0)))
-        self.board[0][7].update_piece(Rook('black', (0, 7)))
-        self.board[7][0].update_piece(Rook('white', (7, 0)))
-        self.board[7][7].update_piece(Rook('white', (7, 7)))
+        self.board[0][0].updatePiece(Rook('black', (0, 0)))
+        self.board[0][7].updatePiece(Rook('black', (0, 7)))
+        self.board[7][0].updatePiece(Rook('white', (7, 0)))
+        self.board[7][7].updatePiece(Rook('white', (7, 7)))
         
-        self.board[0][1].update_piece(Knight('black', (0, 1)))
-        self.board[0][6].update_piece(Knight('black', (0, 6)))
-        self.board[7][1].update_piece(Knight('white', (7, 1)))
-        self.board[7][6].update_piece(Knight('white', (7, 6)))
+        self.board[0][1].updatePiece(Knight('black', (0, 1)))
+        self.board[0][6].updatePiece(Knight('black', (0, 6)))
+        self.board[7][1].updatePiece(Knight('white', (7, 1)))
+        self.board[7][6].updatePiece(Knight('white', (7, 6)))
         
-        self.board[0][2].update_piece(Bishop('black', (0, 2)))
-        self.board[0][5].update_piece(Bishop('black', (0, 5)))
-        self.board[7][2].update_piece(Bishop('white', (7, 2)))
-        self.board[7][5].update_piece(Bishop('white', (7, 5)))
+        self.board[0][2].updatePiece(Bishop('black', (0, 2)))
+        self.board[0][5].updatePiece(Bishop('black', (0, 5)))
+        self.board[7][2].updatePiece(Bishop('white', (7, 2)))
+        self.board[7][5].updatePiece(Bishop('white', (7, 5)))
 
-        self.board[0][3].update_piece(Queen('black', (0, 3)))
-        self.board[7][3].update_piece(Queen('white', (7, 3)))
+        self.board[0][3].updatePiece(Queen('black', (0, 3)))
+        self.board[7][3].updatePiece(Queen('white', (7, 3)))
 
-        self.board[0][4].update_piece(King('black', (0, 4)))
-        self.board[7][4].update_piece(King('white', (7, 4)))
+        self.board[0][4].updatePiece(King('black', (0, 4)))
+        self.board[7][4].updatePiece(King('white', (7, 4)))
 
         self.canvas = tkinter.Canvas(root, width=400, height=400)
         self.canvas.pack()
         
     def display(self):
-        square_size = 50
+        squareSize = 50
         x, y = 0, 0
         
         for row in self.board:
@@ -119,21 +119,21 @@ class ChessBoard:
                 else:
                     color = 'light grey'
                     
-                self.canvas.create_rectangle(x, y, x+square_size, y+square_size, fill=color)
+                self.canvas.create_rectangle(x, y, x+squareSize, y+squareSize, fill=color)
                 
                 if square.piece:
                     if square.piece.color == 'black':
                         color = 'black'
                     else:
                         color = 'white'
-                    self.canvas.create_oval(x, y, x+square_size, y+square_size, fill=color)
+                    self.canvas.create_oval(x, y, x+squareSize, y+squareSize, fill=color)
                 
-                x += square_size
+                x += squareSize
             x = 0
-            y += square_size
+            y += squareSize
 
 
 # define a class for the square
-chess_board = ChessBoard()
-chess_board.display()
+chessBoard = ChessBoard()
+chessBoard.display()
 root.mainloop()
